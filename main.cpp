42e6f67ccf99b23514c79750e303d1e58114c194
Alexandre Henriques <henriquesbr@gmail.com>
2026-05-20 11:31:13 -0300

    copied UDT 1

--- main.cpp
+++ main.cpp
@@ -74,7 +74,114 @@ write 3 UDTs below that EACH have:
 /*
  copied UDT 1:
  */
+struct CoffeeShop
+{
+    int numOfCoffeeMachines;
+    int numOfEmployees;
+    float amountOfCoffeBeanInStorage = 25.f;
+    double dailyProfitAmount =  1500;
+    std::string shopName = "Prenda Cafe";
 
+    CoffeeShop();
+
+    struct CoffeeMachine
+    {
+        std::string machineModelName;
+        float waterTankCapacityInLiters;
+        int numOfEspressoShotsMadeToday = 120;
+        double machineTemperatureInCelsius;
+        bool isSteamWandEnabled = true;
+
+        CoffeeMachine();
+
+        void makeEspresso(int numOfShots);
+        void steamMilk(float milkAmountInMl);
+        bool cleanMachine();
+    };
+    
+    void brewCoffee(CoffeeMachine activeCoffeeMachine, int numOfCups);
+    void serveCustomers(int numOfCustomers);
+    void cleanTables(int numOfTables);
+    void restockCoffeeBeans(float amountToAdd, float maxStorageAmount);
+    
+    CoffeeMachine currentCoffeeMachine;
+};
+
+CoffeeShop::CoffeeMachine::CoffeeMachine() :
+    machineModelName("Perfect Brew"),
+    waterTankCapacityInLiters(3.2f),
+    numOfEspressoShotsMadeToday(0),
+    machineTemperatureInCelsius(92.0),
+    isSteamWandEnabled(true)
+{
+    std::cout << "CoffeeMachine constructed!\n";
+}
+
+void CoffeeShop::CoffeeMachine::makeEspresso(int numOfShots)
+{
+    numOfEspressoShotsMadeToday += numOfShots;
+    std::cout << "Machine Model: " << machineModelName << '\n';
+    std::cout << "Water Tank Capacity: " << waterTankCapacityInLiters << " liters\n\n";
+
+    std::cout << "Making " << numOfShots << " " << (numOfShots == 1 ? "cup" : "cups") << " espresso shots\n";
+}
+
+void CoffeeShop::CoffeeMachine::steamMilk(float milkAmountInMl)
+{
+    std::cout << "Steaming " << milkAmountInMl << " ml of milk\n";
+}
+
+bool CoffeeShop::CoffeeMachine::cleanMachine()
+{
+    std::cout << "Cleaning coffee machine\n";
+
+    return true;
+}
+
+CoffeeShop::CoffeeShop(): numOfCoffeeMachines(6), numOfEmployees(9)
+{
+    std::cout << "CoffeeShop being constructed!" << std::endl;
+}
+
+void CoffeeShop::brewCoffee(CoffeeMachine activeCoffeeMachine, int numOfCups)
+{
+    activeCoffeeMachine.makeEspresso(numOfCups);
+
+    std::cout << "Brewing " << numOfCups << " cups of coffee\n";
+    std::cout << "Shop Name: " << shopName << '\n';
+    std::cout << "Employees: " << numOfEmployees << '\n';
+    std::cout << "Coffee Machines: " << numOfCoffeeMachines << "\n\n";
+}
+
+void CoffeeShop::serveCustomers(int numOfCustomers)
+{
+    std::cout << "Serving " << numOfCustomers << " customers\n";
+}
+
+void CoffeeShop::cleanTables(int numOfTables)
+{
+    std::cout << "Cleaning " << numOfTables << " tables\n";
+}
+
+void CoffeeShop::restockCoffeeBeans(float amountToAdd, float maxStorageAmount)
+{
+    std::cout << "Restocking Coffee Beans\n";
+    std::cout << "Amount in stock " << amountOfCoffeBeanInStorage << " Amount to add " << amountToAdd << " Max storage " << maxStorageAmount << "\n";
+    while (amountOfCoffeBeanInStorage < maxStorageAmount)
+    {
+        amountOfCoffeBeanInStorage += amountToAdd;
+        if (amountOfCoffeBeanInStorage >= maxStorageAmount)
+        {
+            std::cout << "Cannot add more beans.\n";
+            std::cout << "Storage would exceed maximum capacity.\n";
+            return;
+        }
+        else
+        {
+            std::cout << "Current coffee bean storage: " << amountOfCoffeBeanInStorage << " kg\n" ;
+        }
+    }
+}
 /*
  copied UDT 2:
  */
