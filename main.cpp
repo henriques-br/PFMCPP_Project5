99efc5599241476311e4ae1636e848fc249b3d1f
Alexandre Henriques <henriquesbr@gmail.com>
2026-05-20 12:27:34 -0300

    new UDT 5

--- main.cpp
+++ main.cpp
@@ -792,6 +792,22 @@ void RecordingSession::recordTracks(int numOfTracks)
  new UDT 5:
  with 2 member functions
  */
+struct MixingStudio
+{
+    Computer computer;
+    StudioMonitors studioMonitors;
+    MidiKeyboard midiKeyboard;
+
+    int numOfMixesCompleted = 0;
+    double sessionCpuUsagePercentage = 45.0;
+    std::string mixingEngineerName = "Alex";
+
+    MixingStudio();
+    ~MixingStudio();
+
+    void mixSong(std::string songName);
+    void processMixSessions(int numOfSessions);
+};
 
 /*
  MAKE SURE YOU ARE NOT ON THE MASTER BRANCH
