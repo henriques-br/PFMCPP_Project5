6412bd6192c34827db4bdb28bd0680405b2355c3
Alexandre Henriques <henriquesbr@gmail.com>
2026-05-21 17:20:22 -0300

    fixed indentation and added cout to main for 1st UDT

--- main.cpp
+++ main.cpp
@@ -888,42 +888,50 @@ void MixingStudio::processMixSessions(int numOfSessions)
 
 int main()
 {
-     std::cout << "=========== 1st UDT ===========" << std::endl;
-     CoffeeShop coffeeShop;
-     coffeeShop.serveCustomers(12);
-     coffeeShop.cleanTables(5);
-     coffeeShop.currentCoffeeMachine.makeEspresso(3);
-     coffeeShop.currentCoffeeMachine.steamMilk(250.0f);
-     coffeeShop.currentCoffeeMachine.cleanMachine();
-     coffeeShop.brewCoffee(coffeeShop.currentCoffeeMachine, 10);
-     coffeeShop.restockCoffeeBeans(8.f, 55.f);
-
-     std::cout << "=========== 2nd UDT with nested ===========" << std::endl;
-     FitnessTracker fitnessTracker;
-     fitnessTracker.currentWorkoutSession.startWorkout();
-     fitnessTracker.trackWorkout(fitnessTracker.currentWorkoutSession);
-     fitnessTracker.displayNotification("Goal reached");
-     fitnessTracker.monitorSleep(8);
-     fitnessTracker.currentWorkoutSession.pauseWorkout();
-     fitnessTracker.currentWorkoutSession.endWorkout();
-     fitnessTracker.simulateStepGoal(3000, 500);
-
-     std::cout << "=========== 3rd UDT with nested ===========" << std::endl;
-     MusicStudioComputerSetup setup;
-     setup.recordMusic("New Album");
-     setup.editAudioTrack("Lead Vocals");
-     setup.playVirtualInstrument("Synth Pad");
-     setup.runRecordingSessions(4);
-
-     std::cout << "=========== 4th UDT with nested ===========" << std::endl;
-     RecordingSession recordingSession;
-     recordingSession.startSession("Another Day");
-     recordingSession.recordTracks(6);
-
-     std::cout << "=========== 5th UDT with nested ===========" << std::endl;
-     MixingStudio mixingStudio;
-     mixingStudio.mixSong("Thunder");
-     mixingStudio.processMixSessions(4);
-
+    std::cout << "=========== 1st UDT ===========" << std::endl;
+    CoffeeShop coffeeShop;
+    /*
+    coffeeShop.serveCustomers(12);
+    coffeeShop.cleanTables(5);
+    coffeeShop.currentCoffeeMachine.makeEspresso(3);
+    coffeeShop.currentCoffeeMachine.steamMilk(250.0f);
+    coffeeShop.currentCoffeeMachine.cleanMachine();
+    coffeeShop.brewCoffee(coffeeShop.currentCoffeeMachine, 10);
+    coffeeShop.restockCoffeeBeans(8.f, 55.f);
+    */
+    
+    std::cout << "Shop Name: " << coffeeShop.shopName << '\n';
+    std::cout << "Employees: " << coffeeShop.numOfEmployees << '\n';
+    std::cout << "Coffee Machines: " << coffeeShop.numOfCoffeeMachines << "\n";
+    
+    coffeeShop.printCoffeeShopInfo();
+    
+    std::cout << "=========== 2nd UDT with nested ===========" << std::endl;
+    FitnessTracker fitnessTracker;
+    fitnessTracker.currentWorkoutSession.startWorkout();
+    fitnessTracker.trackWorkout(fitnessTracker.currentWorkoutSession);
+    fitnessTracker.displayNotification("Goal reached");
+    fitnessTracker.monitorSleep(8);
+    fitnessTracker.currentWorkoutSession.pauseWorkout();
+    fitnessTracker.currentWorkoutSession.endWorkout();
+    fitnessTracker.simulateStepGoal(3000, 500);
+    
+    std::cout << "=========== 3rd UDT with nested ===========" << std::endl;
+    MusicStudioComputerSetup setup;
+    setup.recordMusic("New Album");
+    setup.editAudioTrack("Lead Vocals");
+    setup.playVirtualInstrument("Synth Pad");
+    setup.runRecordingSessions(4);
+    
+    std::cout << "=========== 4th UDT with nested ===========" << std::endl;
+    RecordingSession recordingSession;
+    recordingSession.startSession("Another Day");
+    recordingSession.recordTracks(6);
+    
+    std::cout << "=========== 5th UDT with nested ===========" << std::endl;
+    MixingStudio mixingStudio;
+    mixingStudio.mixSong("Thunder");
+    mixingStudio.processMixSessions(4);
+    
     std::cout << "good to go!" << std::endl;
 }
